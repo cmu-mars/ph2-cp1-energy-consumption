98271e02203f7598d5673be526e1a35182a051d4
Miguel Velez <miguelvelez@mijecu25.com>
2018-04-12 15:50:57 -0400

    added comments with logic to implement

--- src/main/java/org/sa/rainbow/brass/analyses/ph2/cp1/EnergyConsumptionPredictor.java
+++ src/main/java/org/sa/rainbow/brass/analyses/ph2/cp1/EnergyConsumptionPredictor.java
@@ -4,10 +4,21 @@ import org.sa.rainbow.brass.confsynthesis.Configuration;
 
 public class EnergyConsumptionPredictor {
 
+    // TODO check the signature
     public double energy(Configuration config, double time, String configID) {
-throw new UnsupportedOperationException("Implement");
+        // TODO get energy of current configuration
+        // TODO multiply the energy by the time to do all tasks
+        // TODO return energy that will be consumed
+        throw new UnsupportedOperationException("Implement");
     }
 
+    /**
+     * Check if the task can be completed based on the energy that will be consumed and the current energy.
+     *
+     * @param curEnergy
+     * @param energyConsumed
+     * @return
+     */
     public boolean canCompleteTask(double curEnergy, double energyConsumed) {
         return curEnergy >= energyConsumed;
     }
